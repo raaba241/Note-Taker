e77a1b33d603a1996766fa7fed761695879d0591
unknown <raaba21@gmail.com>
2024-02-02 19:05:07 -0500

    added routing that actually works and retrieved current database information

--- routes/notes.js
+++ routes/notes.js
@@ -1,2 +1,31 @@
 const notes = require ('express').Router()
 const { v4: uuidv4 } = require ('uuid')
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+notes.get ('/',(req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+})
+
+notes.post('/', (req, res) => {
+    
+    const { title, text } = req.body
+    
+    if (req.body){
+        const newNote = {
+            title,
+            text
+        };
+
+        readAndAppend(newNote, './db/db.json')
+        res.json(`Note Added Successfully`)
+    } else {
+        res.error('Error adding new note')
+    }
+})
+
+
+module.exports = notes;
